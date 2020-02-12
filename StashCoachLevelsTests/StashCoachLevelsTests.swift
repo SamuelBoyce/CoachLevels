21a01e351164ecab4e19020b49f97c195ae9c11a
SamuelBoyce <samuel.boyce@gmail.com>
2020-02-12 13:46:33 -0500

    Initial Commit

---
+++ StashCoachLevelsTests/StashCoachLevelsTests.swift
@@ -0,0 +1,34 @@
+//
+//  StashCoachLevelsTests.swift
+//  StashCoachLevelsTests
+//
+//  Created by Samuel Boyce on 2/12/20.
+//  Copyright © 2020 Samuel Boyce. All rights reserved.
+//
+
+import XCTest
+@testable import StashCoachLevels
+
+class StashCoachLevelsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
